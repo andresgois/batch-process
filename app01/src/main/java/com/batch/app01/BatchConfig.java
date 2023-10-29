728ae85520e19b3c291e589142812ac6deb63c25
andre <andresousalee@gmail.com>
2023-10-28 23:44:50 -0300

    🐞 fix: correcao no autowired

--- app01/src/main/java/com/batch/app01/BatchConfig.java
+++ app01/src/main/java/com/batch/app01/BatchConfig.java
@@ -7,7 +7,6 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,18 +15,23 @@ import org.springframework.context.annotation.Configuration;
 @EnableBatchProcessing // para trazer todas as features do spring batch, é necessário habilita-lo
 @Configuration // necessário ser uma classe de configuração
 public class BatchConfig {
- 
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
 
+    private final JobBuilderFactory jobBuilderFactory;
     // informa que um objeto existe para o contexto, pra ser injetado
+    private final StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    public BatchConfig(JobBuilderFactory jobBuilderFactory,
+                       StepBuilderFactory stepBuilderFactory){
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+
     @Bean // retorna um job
     public Job imprimeOlaJob() {
         return jobBuilderFactory
                 .get("imprimeOlaJob")// nome do job
-                /**
+                /*
                  * qual a logica do job, o job é dividido em etapas que são chamado de steps,
                  * steps são encadeados para contér um lógica maior
                  */
@@ -38,7 +42,14 @@ public class BatchConfig {
     public Step imprimeOlaStep() {
         return stepBuilderFactory
                 .get("imprimeOlaStep") // todos começam com get, que é o nome do step
-                .tasklet(new Tasklet() { // pequenas tarefas, que necessitam de pouco poder no processamento
+                .tasklet( (StepContribution contribution,
+                           ChunkContext chunkContext) -> {
+                    System.out.println("Olá mundo");
+                    System.out.println("Executa a lógica");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+                /*.tasklet(new Tasklet() { // pequenas tarefas, que necessitam de pouco poder no processamento
                     
                     @Override
                     public RepeatStatus execute(StepContribution contribution,
@@ -47,6 +58,6 @@ public class BatchConfig {
                         System.out.println("Executa a lógica");
                         return RepeatStatus.FINISHED; // indica que a tarefa terminou
                     }
-                }).build();
+                }).build();*/
     }
 }
